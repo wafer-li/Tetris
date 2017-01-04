7c733373886e969120438191105ed25b767cb8ef
李东霖 <omyshokami@gmail.com>
2017-01-04 16:03:42 +0800

    Add tcp

---
+++ MyTetris/TcpP1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyTetris
+{
+    static class TcpP1
+    {
+        private static TcpListener tcpLister;
+
+        public static TcpClient TcpClient { get; private set; }
+        public static BinaryReader Reader { get; private set; }
+        public static BinaryWriter Writer { get; private set; }
+
+        public static Thread acceptThread;
+
+        public static void Start()
+        {
+            tcpLister = new TcpListener(IPAddress.Parse("127.0.0.1"), 23333);
+            tcpLister.Start();
+            // 启动一个线程来接受请求 
+            acceptThread = new Thread(AcceptClientConnect);
+            acceptThread.Start();
+        }
+
+        private static void AcceptClientConnect()
+        {
+            Thread.Sleep(1000);
+            try
+            {
+                TcpClient = tcpLister.AcceptTcpClient();
+                if (tcpLister != null)
+                {
+                    NetworkStream networkStream = TcpClient.GetStream();
+                    Reader = new BinaryReader(networkStream);
+                    Writer = new BinaryWriter(networkStream);
+                }
+            }
+            catch
+            {
+                Stop();
+            }
+        }
+
+        public static void Stop()
+        {
+            Reader.Close();
+            Writer.Close();
+            tcpLister.Stop();
+            acceptThread.Abort();
+        }
+
+        public static void SendMessage(object state)
+        {
+            try
+            {
+                Writer.Write(state.ToString());
+                Writer.Flush();
+            }
+            catch
+            {
+                if (Reader != null)
+                {
+                    Reader.Close();
+                }
+                if (Writer != null)
+                {
+                    Writer.Close();
+                }
+                if (TcpClient != null)
+                {
+                    TcpClient.Close();
+                }
+
+            }
+        }
+
+        public static string getMessage()
+        {
+            return Reader.ReadString();
+        }
+
+    }
+}
---
+++ MyTetris/TcpP2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyTetris
+{
+    static class TcpP2
+    {
+        private static TcpListener tcpLister;
+
+        public static BinaryReader Reader { get; private set; }
+        public static BinaryWriter Writer { get; private set; }
+        public static TcpClient TcpClient { get; private set; }
+ 
+        public static Thread acceptThread;
+        private static NetworkStream networkStream;
+
+        public static void Start()
+        {
+            Thread connectThread = new Thread(ConnectToServer);
+            connectThread.Start();
+        }
+
+        private static void ConnectToServer()
+        {
+            try
+            {
+                IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+                TcpClient = new TcpClient();
+                TcpClient.Connect(ipaddress, 23333);
+
+                if (TcpClient != null)
+                {
+                    networkStream = TcpClient.GetStream();
+                    Reader = new BinaryReader(networkStream);
+                    Writer = new BinaryWriter(networkStream);
+                }
+
+
+            }
+            catch
+            {
+                Stop();
+            }
+        }
+        public static void Stop()
+        {
+            Reader.Close();
+            Writer.Close();
+            tcpLister.Stop();
+            acceptThread.Abort();
+        }
+
+        public static void SendMessage(object state)
+        {
+            try
+            {
+                Writer.Write(state.ToString());
+                Writer.Flush();
+            }
+            catch
+            {
+                if (Reader != null)
+                {
+                    Reader.Close();
+                }
+                if (Writer != null)
+                {
+                    Writer.Close();
+                }
+                if (TcpClient != null)
+                {
+                    TcpClient.Close();
+                }
+
+            }
+        }
+
+        public static string getMessage()
+        {
+            return Reader.ReadString();
+        }
+
+    }
+}
